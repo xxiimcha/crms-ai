dae2e58fff9bbbefed3ce5414733393c6a10fcb5
Charmaine Cator <charmaine.l.d.cator@gmail.com>
2025-02-25 16:47:54 +0800

    Added function per module

--- index.php
+++ index.php
@@ -1,40 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Login</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    <style>
-        .card {
-            border-radius: 15px;
-        }
-        .btn-user {
-            font-size: 1.1rem;
-            padding: 12px;
-        }
-        .form-control-user {
-            border-radius: 10px;
-        }
-        .login-container {
-            max-width: 450px;
-            margin: auto;
-        }
-    </style>
-</head>
-
-<body class="bg-gradient-primary">
+<?php include('partials/head.php');?>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-6 login-container">
@@ -65,12 +30,4 @@
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="js/sb-admin-2.min.js"></script>
-</body>
-
-</html>
+<?php include('partials/foot.php');?>
